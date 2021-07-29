8d989ccbada3fc4eeb3275d1a701d889f31195aa
sbalexandru <sbalexandru@yahoo.com>
2021-07-29 16:59:14 +0300

    first uploade

---
+++ app.js
@@ -0,0 +1,6 @@
+const valEcran = document.querySelectorAll(".ecranInitial");
+const numar = document.querySelectorAll(".sapte");
+
+numar.addEventListener("keyup", function () {
+  paragraph.innerNumber = numar.value;
+});
